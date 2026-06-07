f325da2b741419e4fe1aa9d6c22180649265aff3
vito1317 <service@vito1317.com>
2026-06-07 20:34:06 +0800

    補回漏掉的 withStyle import（修 Markdown 編譯）

--- app/src/main/java/com/vito/gateway/Markdown.kt
+++ app/src/main/java/com/vito/gateway/Markdown.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.sp
 
